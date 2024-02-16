cb9564b5ca0312e074e4a5bb9c94af526b8b0369
momoiroshikibu <momoiroshikibu@gmail.com>
2024-02-16 16:51:41 +0900

    convert

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,58 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "clsx-className" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tailwind-clsx" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    `clsx-className.convert`,
+    (event) => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      const activeEditor = vscode.window.activeTextEditor;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tailwind-clsx.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from tailwind-clsx!');
-	});
+      if (activeEditor === undefined) {
+        return;
+      }
+      convert(activeEditor);
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
+}
+
+function convert(editor: vscode.TextEditor) {
+  editor.edit((builder) => {
+    const doc = editor.document;
+    const originalText = doc.getText();
+    builder.replace(
+      new vscode.Range(
+        doc.lineAt(0).range.start,
+        doc.lineAt(doc.lineCount - 1).range.end
+      ),
+      originalText.replace(
+        /className="([^"]*)"/g,
+        (match, classNames: string) => {
+          // クラス名をスペースで分割し、clsxの引数形式に変換
+          const clsxArgs = classNames
+            .split(" ")
+            .map((className) => `"${className}"`)
+            .join(", ");
+          return `className={clsx(${clsxArgs})}`;
+        }
+      )
+    );
+  });
 }
 
 // This method is called when your extension is deactivated
